6bc7d7451040b5781e534e2cf818b902e4b242a5
the-listener <deathakanote@gmail.com>
2017-01-19 14:55:35 +0530

    Some changes regarding the URL

--- app/src/main/java/com/example/martian/quakereport/EarthQuakeActivity.java
+++ app/src/main/java/com/example/martian/quakereport/EarthQuakeActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,14 +15,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EarthQuakeActivity extends AppCompatActivity {
-    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query";
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+
     private EarthQuakeAdapter earthquakeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earth_quake);
-        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
+
 
 
         earthquakeAdapter = new EarthQuakeAdapter(this, new ArrayList<Earthquake>());
@@ -37,27 +38,30 @@ public class EarthQuakeActivity extends AppCompatActivity {
                 startActivity(websiteIntent);
             }
         });
-
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
     }
         private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
             @Override
             protected List<Earthquake> doInBackground(String... urls) {
+
                 if (urls.length <= 0 || urls[0] == null) {
                     return null;
                 }
-
                 List<Earthquake> result =  QueryUtils.fetchEarthquakeData(urls[0]);
                 return result;
             }
+            @Override
+            protected void onPostExecute(List<Earthquake> data){
+                earthquakeAdapter.clear();
+                if(data!=null && !data.isEmpty()){
+                    earthquakeAdapter.addAll(data);
+                }
+            }
+
 
         }
 
-    protected void onPostExecute(List<Earthquake> data) {
-        earthquakeAdapter.clear();
-        if (data != null && !data.isEmpty()) {
-            earthquakeAdapter.addAll(data);
-        }
-    }
 }
 
 
